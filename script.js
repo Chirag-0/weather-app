467104c26498b7e8a68692f3aaf553b1cf094ef8
Chirag-0 <123808423+Chirag-0@users.noreply.github.com>
2024-08-01 22:26:02 +0530

    Initial commit

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
+    console.log(data);
 
     if (response.status == 404) {
         document.querySelector(".error").style.display = "block";
@@ -22,19 +23,19 @@ async function checkWeather(city) {
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-        if (data.weather[0].description == "clouds" || data.weather[0].description == "scattered clouds") {
+        if (data.weather[0].description == "Clouds" || data.weather[0].description == "scattered clouds") {
             weatherIcon.src = "images/clouds.png";
         }
-        else if (data.weather[0].description == "clear") {
+        else if (data.weather[0].main == "Clear") {
             weatherIcon.src = "images/clear.png";
         }
-        else if (data.weather[0].description == "rain") {
+        else if (data.weather[0].main == "Rain") {
             weatherIcon.src = "images/rain.png";
         }
-        else if (data.weather[0].description == "haze") {
+        else if (data.weather[0].main == "Haze") {
             weatherIcon.src = "images/drizzle.png";
         }
-        else if (data.weather[0].description == "mist") {
+        else if (data.weather[0].main == "Mist") {
             weatherIcon.src = "images/mist.png";
         }
 
